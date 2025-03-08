0ff1aa88bf87faf202d8811b0618b7b069e3fa92
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-03-08 13:49:20 -0600

    styling changes to cards

--- cd/src/pages/education/index.js
+++ cd/src/pages/education/index.js
@@ -42,12 +42,17 @@ const EducationPage = () => {
               <div className="certCard">
                 <div className="certCard-inner">
 
-                  <div className="certCard-front">
+                  <div className="certCard-front brdrlft">
                     <div>
-                      <h3>{cert.name}</h3>
-                      <p>{cert.distributor}</p>
-                      {cert?.issued ? <p>Issued: {cert.issued}</p> : <p>''</p>}
-                      {cert?.expiration ? <p>Expires: {cert.expiration}</p> : <p>Expires: Does not expire.</p>}
+                      <h3 className="brdrbtm h3title"><span>{cert.name}</span></h3>
+                      <p className="brdrbtm brdrbtm-height"><span>{cert.distributor}</span></p>
+                      {cert?.issued ? <p className="brdrbtm brdrbtm-height"><span>Issued: {cert.issued}</span></p> : <p className="brdrbtm"><span>''</span></p>}
+                      {cert?.expiration ? <p className="brdrbtm brdrbtm-height"><span>Expires: {cert.expiration}</span></p> : <p className="brdrbtm"><span>Expires: Does not expire.</span></p>}
+                      <p className="brdrbtm brdrbtm-height"></p>
+                      <p className="brdrbtm brdrbtm-height"></p>
+                      <p className="brdrbtm brdrbtm-height"></p>
+                      <p className="brdrbtm brdrbtm-height"></p>
+
                     </div>
                   </div>
                   <div className="certCard-back"><div id={cert.id}></div>
