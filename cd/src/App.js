85639cbb7e5769087f043f563b969a7f93e759ed
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-04-15 05:51:15 -0500

    general OG image

--- cd/src/App.js
+++ cd/src/App.js
@@ -3,17 +3,21 @@ import * as React from 'react';
 import Header from "./components/header/header"
 import Footer from "./components/footer/footer"
 import { Outlet } from 'react-router';
-import { HelmetProvider } from 'react-helmet';
-
+import { Helmet, HelmetProvider } from 'react-helmet';
+import ogImage from "./assests/images/cooltechbg.webp";
 
 const App = () => {
   return (
-    
-    <div className="App">
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+    <>
+      <Helmet>
+      <meta property="og:image" content={ogImage}/>
+      </Helmet>
+      <div className="App">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </>
   );
 }
 
