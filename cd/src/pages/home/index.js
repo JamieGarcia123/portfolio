3f5414c3364d77028d51e511c85f5d969865cc30
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-04-14 19:53:38 -0500

    home page redesign

--- cd/src/pages/home/index.js
+++ cd/src/pages/home/index.js
@@ -1,17 +1,17 @@
 import './homepage.css'
 import Logos from "../../components/logos/logos";
-import codePic from "../../assests/images/certcardcode.webp"
+import video from "../../assests/images/home-video-clip.mp4"
 import { Helmet } from 'react-helmet';
 
 
 const responsive = {
   desktop: {
     breakpoint: { max: 6000, min: 1024 },
-    items: 3
+    items: 6
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2
+    items: 4
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
@@ -24,26 +24,28 @@ const HomePage = () => {
             <Helmet>
                 <title>Jamie Garcia | Software Developer</title>
             </Helmet>
-            <header id="homeHero" className="hero">
-                <div className='heroContainer'>
-                    <div id="home" className='col1 text-center'>
+            <video playsinline autoPlay muted loop id="myVideo">
+                <source src={video} type="video/mp4"/>
+                Your browser does not support HTML5 video.
+            </video>   
+                <section id="homeHero" className="homeHeroContainer">
+                <div className='row container '>
+                    <div className='col1'>
+                   <Logos responsive={responsive} centered={true} title="Skills"/>
+                   </div>
+                   </div>
+                   <div className='row container home-font '>
+                   <div  id="slide" className='text-left'>
                         <h1>Jamie Garcia</h1>
                         <h2>Software Developer</h2>
-                    </div>
-                </div>
-            </header>
-            <section id="homeContent" className="container">
-                <div className='col3 center-items text-center padding-25'>
-                    <h3 className='no-margin'>Welcome!</h3>
-                    <p>A motivated Web Developer with 5 years of experience in JavaScript frameworks such Vue.js with Nuxt, React with and without NextJs. Familiar with TypeScript, Redux and Angular.</p>
-                    <p>Also experienced in C# and .Net Object Oriented projects. Skilled in WordPress (Elementor, Avada, WooCommerce), HTML, CSS, SCSS, TailWinds CSS, Bootstrap, and REST APIs. Strong background in mobile responsivenss. Familiar with Python, C#, SQL, and Agile project management.</p>
-                    <p> Have worked with workflows in Jira, Monday and Microsoft Teamworks. Am efficient in Remote settings.</p>                        
-                    <p>Currently pursuing a Bachelor’s in Computer Science at WGU.</p>
-                </div>
-                <div className='col2-3 text-center'>
-                    <img className='m-auto codeImg' alt="Screen shot of code used for this site." src={codePic} height={300} width={800}/>
-                    <Logos responsive={responsive} centered={true} title="Skills"/>
-                </div>
+                    </div> 
+                    <div className='col2 center-items text-center padding-25'>
+                        <p>A motivated Web Developer with 5 years of experience in JavaScript frameworks such Vue.js with Nuxt, React with and without NextJs. Familiar with TypeScript, Redux and Angular.</p>
+                        <p>Also experienced in C# and .Net Object Oriented projects. Skilled in WordPress (Elementor, Avada, WooCommerce), HTML, CSS, SCSS, TailWinds CSS, Bootstrap, and REST APIs. Strong background in mobile responsivenss. Familiar with Python, C#, SQL, and Agile project management.</p>
+                        <p> Have worked with workflows in Jira, Monday and Microsoft Teamworks. Am efficient in Remote settings.</p>                        
+                        {/* <img className='m-auto codeImg' alt="Screen shot of code used for this site." src={codePic} height={300} width={800}/> */}
+                    </div> 
+                </div> 
             </section>
         </>
     )
