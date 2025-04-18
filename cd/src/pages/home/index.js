536b98211556445401863c8c428c6ce07125a35c
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-04-17 20:50:01 -0500

    adjusting layout to help with mobile issues, tweaking CSS and html structure

--- cd/src/pages/home/index.js
+++ cd/src/pages/home/index.js
@@ -27,30 +27,30 @@ const HomePage = () => {
                 <title>Jamie Garcia | Software Developer</title>
                 <meta property="og:image" content={ogImage}/>
             </Helmet>
-            <section id="homeHero" className="homeHeroContainer">
+            <header className="homeHeroContainer">
 
             <video playsinline autoPlay muted loop id="myVideo">
                 <source src={video} type="video/mp4"/>
                 Your browser does not support HTML5 video.
-            </video>   
-                <div className='row container '>
-                    <div className='col1'>
-                   <Logos responsive={responsive} centered={true} title="Skills"/>
-                   </div>
-                   </div>
-                   <div className='row home-font gradientBG '>
-                   <div id="slide" className='text-left'>
-                        <h1>Jamie Garcia</h1>
-                        <h2>Software Developer</h2>
-                    </div> 
-                    <div className='col2 center-items text-center padding-25'>
-                        <p>A motivated Web Developer with 5 years of experience in JavaScript frameworks such Vue.js with Nuxt, React with and without NextJs. Familiar with TypeScript, Redux and Angular.</p>
-                        <p>Also experienced in C# and .Net Object Oriented projects. Skilled in WordPress (Elementor, Avada, WooCommerce), HTML, CSS, SCSS, TailWinds CSS, Bootstrap, and REST APIs. Strong background in mobile responsivenss. Familiar with Python, C#, SQL, and Agile project management.</p>
-                        <p> Have worked with workflows in Jira, Monday and Microsoft Teamworks. Am efficient in Remote settings.</p>                        
-                        {/* <img className='m-auto codeImg' alt="Screen shot of code used for this site." src={codePic} height={300} width={800}/> */}
-                    </div> 
-                </div> 
-           </section>
+            </video>    
+            </header> 
+            <section >
+              <Logos responsive={responsive} centered={true} title="Skills"/>
+           
+              <div className='row home-font gradientBG'>
+                <div className='col2'>
+                  <div id="slide" className='text-left'>
+                      <h1>Jamie Garcia</h1>
+                      <h2>Software Developer</h2>
+                  </div> 
+                </div>
+                <div className='col2 center-items text-center padding-25'>
+                    <p>A motivated Web Developer with 5 years of experience in JavaScript frameworks such Vue.js with Nuxt, React with and without NextJs. Familiar with TypeScript, Redux and Angular.</p>
+                    <p>Also experienced in C# and .Net Object Oriented projects. Skilled in WordPress (Elementor, Avada, WooCommerce), HTML, CSS, SCSS, TailWinds CSS, Bootstrap, and REST APIs. Strong background in mobile responsivenss. Familiar with Python, C#, SQL, and Agile project management.</p>
+                    <p> Have worked with workflows in Jira, Monday and Microsoft Teamworks. Am efficient in Remote settings.</p>                        
+                </div>
+              </div> 
+            </section>
         </>
     )
 }
