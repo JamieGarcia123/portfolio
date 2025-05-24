b4543ebac3a893e5ce5285488e48e46c6a8b3e76
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-05-23 20:54:02 -0500

    more content added

--- cd/src/pages/education/index.js
+++ cd/src/pages/education/index.js
@@ -36,6 +36,9 @@ const EducationPage = () => {
       </header>
       <section id="education" className="certContainer">
         <div className="col2">
+             <p>My journey into web development began as a self-taught learner, driven by curiosity and a desire to create interactive, meaningful digital experiences. Through countless hours of exploring online resources, building small projects, and experimenting with code, I developed a strong foundation in web technologies and problem-solving. This passion eventually led me to pursue a formal education at <strong>California State University, Northridge (CSUN)</strong>, where I earned a <strong>Bachelor of Science in Software Engineering</strong> Combining hands-on experience with academic training has given me a well-rounded skill set that blends creativity with technical precision, allowing me to build user-focused, visually compelling, and high-performing web applications.
+</p>
+
           <div className="certContent padding-25 ">
             {educationalItems.map(item => (
               <div key={item.id} className="certContent">
